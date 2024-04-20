658915e2f8db6e9650bb754ca565b67f41903cc0
Mateja Tasic <70795666+matejatasic@users.noreply.github.com>
2024-04-20 14:02:33 +0200

    move home page to pages folder

--- cab-managemeny-system-spa/src/App.tsx
+++ cab-managemeny-system-spa/src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.scss';
-import Home from './home/Home';
+import Home from './pages/home/Home';
 import Navbar from './common/Navbar';
 
 function App() {
