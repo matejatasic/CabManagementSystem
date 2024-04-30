5cced0db79995547406914de52e99c7c387cd9a6
Mateja Tasic <70795666+matejatasic@users.noreply.github.com>
2024-04-30 15:05:55 +0200

    add employees page

---
+++ cab-managemeny-system-spa/src/pages/admin/employees/Employees.tsx
@@ -0,0 +1,32 @@
+import ContentCard from "../common/content-card/ContentCard";
+import TableContent from "../common/table-content/TableContent";
+
+export default function Employees() {
+    const headers = ["Username", "Email", "Full Name", "Address", "Phone number", "Branch", "Date joined"];
+    const rows = [
+        [
+            "brianp",
+            "bibendum.fermentum.metus@hotmail.net",
+            "Brian Perez",
+            "7340 Mollis Street, Veracruz, Mexico",
+            "(821) 838-4026",
+            "Travel",
+            "2023-04-12"
+        ],
+        [
+            "joshow",
+            "nibh@outlook.com",
+            "Josiah Howell",
+            "Ap #516-2320 Non, Road, New Brunswick, USA",
+            "(672) 350-7355",
+            "Finance",
+            "2023-01-22"
+        ],
+    ];
+
+    return (
+        <ContentCard>
+            <TableContent headers={headers} rows={rows} />
+        </ContentCard>
+    );
+}
